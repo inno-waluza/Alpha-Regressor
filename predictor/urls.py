3c3cc40f5e011770e0de3059ced5163acba3d6b3
inno-waluza <innowaluza@gmail.com>
2024-08-21 14:08:36 +0200

    removed minot bug in urls

--- predictor/urls.py
+++ predictor/urls.py
@@ -5,5 +5,5 @@ from .views import HomePageView, PredictPageView, AboutPageView
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('predict/', PredictPageView.as_view(), name='predict'),
-    path('/about',AboutPageView.as_view(), name='about'),
+    path('about/',AboutPageView.as_view(), name='about'),
 ]
